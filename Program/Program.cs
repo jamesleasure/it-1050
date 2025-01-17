b5bf0deb42974e6e3ea63686c179b63e2eb1cc4b
James Leasure <james.leasure@tri-c.edu>
2025-01-17 05:47:20 +0000

    Add debugging configuration for .NET 8.0 and devcontainer setup

---
+++ Program/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 10;
+        Console.WriteLine(num);
+        Console.WriteLine("Hello, World!");
+    }
+}
